3da5b72cecf031c5880ef9e6dbbc3bb225d53ca5
Alex Roman <romanalexrares@gmail.com>
2025-02-04 20:46:56 +0200

    04.02.25: Popups

--- platforma-romana/src/components/QuizBox.tsx
+++ platforma-romana/src/components/QuizBox.tsx
@@ -7,6 +7,7 @@ const QuizBox: React.FC<QuickQuestionProps> = ({quizQuestions, user}) => {
   const [currentAnswers, setCurrentAnswers] = useState<string[]>();
   const [selectedAnswer, setSelectedAnswer] = useState<string | null>();
   const [isCorrect, setIsCorrect] = useState<boolean>();
+  const [showPopup, setShowPopup] = useState(false);
     
   const handleCheckAnswer = () => {
     if (selectedAnswer !== undefined) {
@@ -17,6 +18,7 @@ const QuizBox: React.FC<QuickQuestionProps> = ({quizQuestions, user}) => {
         if (selectedAnswer == currentQuestion.Correct) {
           fetchUpdateGold(user, 5);
           fetchUpdateExperience(user, 50);
+          triggerPopup();
         }
       }
     }
@@ -33,6 +35,13 @@ const QuizBox: React.FC<QuickQuestionProps> = ({quizQuestions, user}) => {
     setSelectedAnswer(undefined);
   }
 
+  const triggerPopup = () => {
+    setShowPopup(true);
+    setTimeout(() => {
+      setShowPopup(false);
+    }, 1900);
+  };
+  
   useEffect(() => {
     if (currentQuestion) {
       setCurrentAnswers([
@@ -51,6 +60,7 @@ const QuizBox: React.FC<QuickQuestionProps> = ({quizQuestions, user}) => {
   return (
     <div className="quizz-box">
       <h2> Întrebări rapide referitoare la lecție </h2>
+     {showPopup && <div className="popup-green"> Ai primit 5 gold și 50 exp.</div>}
       { currentQuestion && currentAnswers &&
         <div className={`quik-quizz ${(isCorrect === undefined || !selectedAnswer) ? "" : isCorrect ? "correct" : "incorrect"}`}>
         <h3>{currentQuestion.Question}</h3>
